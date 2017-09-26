02de2bc8372c65442f4beb2d0b72025291a70139
Romanow <romanowalex@mail.ru>
2017-09-26 14:15:13 +0300

    add cert

--- src/main/java/ru/romanow/deploy/Application.java
+++ src/main/java/ru/romanow/deploy/Application.java
@@ -1,15 +1,35 @@
 package ru.romanow.deploy;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.UpgradeProtocol;
+import org.apache.coyote.http2.Http2Protocol;
+import org.apache.coyote.http2.Http2UpgradeHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.EmbeddedServletContainerAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * Created by romanow on 01.09.17.
  */
 @SpringBootApplication
-public class Application {
+public class Application
+        extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public TomcatEmbeddedServletContainerFactory tomcatEmbeddedServletContainerFactory() {
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> connector.addUpgradeProtocol(new Http2Protocol()));
+        return factory;
     }
 }
--- src/main/java/ru/romanow/deploy/SimpleController.java
+++ src/main/java/ru/romanow/deploy/SimpleController.java
@@ -1,10 +1,12 @@
 package ru.romanow.deploy;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
 /**
@@ -13,8 +15,15 @@ import java.util.HashMap;
 @Controller
 public class SimpleController {
 
+    /**
+     * openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
+     * openssl pkcs12 -export -in cert.pem -inkey key.pem -out cert_and_key.p12 -CAfile chain.pem -caname root
+     * keytool -importkeystore -deststorepass secret -destkeypass secret -destkeystore keystore.jks -srckeystore cert_and_key.p12 -srcstoretype PKCS12 -srcstorepass secret
+     * keytool -import -trustcacerts -alias root -file chain.pem -keystore keystore.jks
+     */
     @GetMapping
-    public ModelAndView helloWorld(@RequestParam(value = "name", required = false, defaultValue = "world") String name) {
+    public ModelAndView helloWorld(@RequestParam(value = "name", required = false, defaultValue = "world") String name, HttpServletResponse response) {
+        response.setHeader(HttpHeaders.CACHE_CONTROL, "no-cache");
         return new ModelAndView("index", new HashMap<String, String>() {{
             put("name", name);
         }});
